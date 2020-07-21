5efceaeb2632a2b41c8860b18646246e4e64e5f8
DariusPirvulescu <organicdarius@gmail.com>
2020-07-21 23:13:54 +0100

    added basic counter

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
-function App() {
+
+const App = () => {
+  const [count, setCount] = useState(10)
+
+  // console.log("count", count);
+
+  const increase = () => {
+    setCount(count + 1)
+  }
+
+  const decrease = () => {
+    setCount(count - 1)
+  }
+
   return (
     <div className="container">
-      <h1>0</h1>
-      <button>+</button>
+      <h1>{count}</h1>
+      <button onClick={decrease}>-</button>
+      <button onClick={increase}>+</button>
     </div>
   );
 };
+
+// same for using a Class
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="container">
+//         <h1>0</h1>
+//         <button>+</button>
+//       </div>
+//     );
+//   }
+// }
+
 
 export default App;
