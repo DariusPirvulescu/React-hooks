c42847494109f23a276cf32cde6d8b2f26eab6d5
DariusPirvulescu <organicdarius@gmail.com>
2021-01-18 20:58:09 +0200

    Explain when useEffect runs Some optional description, Commit body

--- src/components/customHooks/useFetchUser.js
+++ src/components/customHooks/useFetchUser.js
@@ -17,3 +17,21 @@ export const useFetchUser = (url) => {
 
   return { data, loading };
 };
+
+/**
+ * When useEffect runs:
+ * useEffect(cb, x)
+ * x is always an Array []
+ *
+ * x === []
+ * cb runs only once, during 1st render
+ *
+ * x === null
+ * cb runs during initial render and after
+ * every rerender
+ *
+ * x === [data]
+ * cb runs during initial render and
+ * (every rerender + after data value changes)
+ *
+ */
